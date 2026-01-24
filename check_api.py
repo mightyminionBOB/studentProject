581b719266d6bcce5b4adea337f79a6da366f120
mightyminionBOB <surovfa77@gmail.com>
2026-01-24 06:49:42 +0300

    Update check_api.py

--- check_api.py
+++ check_api.py
@@ -1,3 +1,5 @@
+import time
+
 from services.alpha_vantage import (
     get_global_quote,
     get_daily_series,
@@ -6,9 +8,9 @@ from services.alpha_vantage import (
 
 print("=== PRICE ===")
 print(get_global_quote("AAPL"))
-
+time.sleep(65) # Запросы к API доступны в режиме – 1 запрос в минуту
 print("\n=== DAILY ===")
 print(get_daily_series("AAPL"))
-
+time.sleep(65)
 print("\n=== RSI ===")
 print(get_rsi("AAPL"))
